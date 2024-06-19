756cb7b2e85bac81e38ef80fa5b457607e2602cf
edward-elrics-cool <158344142+edward-elrics-cool@users.noreply.github.com>
2024-06-19 21:21:16 +0100

    Update youtube download.py

--- youtube download.py
+++ youtube download.py
@@ -3,7 +3,7 @@ from pytube import YouTube
 
 
 url = input("Enter URL for video: ")
-location = "C:/Users/mcken/Documents/python scripts/yt_downloader/iles"
+location = "C:/Users/@@@/Documents/python scripts/yt_downloader/iles"
 yt = YouTube(url).streams.first().download(location)
 
 
